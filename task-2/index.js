a9cbffa0b90e6d5aaff2a8e5e82f7bbfef6766cb
Katarhu <mkaimg1@gmail.com>
2023-02-18 17:21:42 +0200

    task-2

---
+++ task-2/index.js
@@ -0,0 +1,46 @@
+// TASK #2
+// Write a function that replaces all ones with zeros and vice versa for the object
+
+const invertValue = (value) => {
+    if (typeof value !== "number" || !Number.isFinite(value)) {
+        return 0;
+    }
+
+    return value === 0 ? 1 : 0;
+}
+
+const foo = (object) => {
+    if (typeof object.left === "object") {
+        object.left = foo(object.left);
+    } else {
+        object.left = invertValue(object.left);
+    }
+
+    if (typeof object.right === 'object') {
+        object.right = foo(object.right);
+    } else {
+        object.right = invertValue(object.right);
+    }
+
+    return object;
+}
+
+const d = {
+    left: {
+        left: {
+            left: 1,
+            right: {
+                left: 0,
+                right: 1,
+            },
+        },
+        right: {
+            left: 0,
+            right: 1,
+        },
+    },
+    right: 5,
+};
+
+foo(d);
+console.log(d);
